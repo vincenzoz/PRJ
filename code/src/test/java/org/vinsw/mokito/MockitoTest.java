408fd0b943899480aaed3e15a4c1955481b5fee5
Vincenzo Zecchillo <vin.zecchillo@gmail.com>
2019-03-29 14:31:26 +0100

    first commit

---
+++ code/src/test/java/org/vinsw/mokito/MockitoTest.java
@@ -0,0 +1,15 @@
+package org.vinsw.mokito;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MockitoTest {
+
+    @Test
+    public void SimpleTest() {
+
+    }
+
+}

e3f91b2c7ec15e6d79fe5e295c62df99627cc8bd
Vincenzo Zecchillo <vin.zecchillo@gmail.com>
2019-04-20 01:03:48 +0200

    added MessageResource draft

--- code/src/main/java/org/vinsw/rest/MessageResource.java
+++ code/src/main/java/org/vinsw/rest/MessageResource.java
@@ -2,12 +2,17 @@ package org.vinsw.rest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vinsw.security.builder.UserBuilder;
+import org.vinsw.security.entity.User;
 
 @RestController
 public class MessageResource {
 
+    int i = 0;
     @RequestMapping(path = "/message")
-    public String getMessage() {
-        return "Hello";
+    public User getMessage() {
+        System.out.println("Req: " + i++);
+        User u = new UserBuilder().setId(Long.valueOf(1)).setPassword("secret").setUsername("user").build();
+        return u;
     }
 }
